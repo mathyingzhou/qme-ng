d065be4353a02cd9c69f7a7f7e6fc0f6d0b4b4af
Ying Zhou <yingzhou474@gmail.com>
2018-06-23 17:49:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ curl -o gmp.tar.bz2 -k https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
 bzip2 -d gmp.tar.bz2
 tar xopf gmp.tar
 cd gmp-6.1.2
-bash -x ./configure --enable-cxx
+bash ./configure --enable-cxx
 make install
 make check
 cd ..
@@ -22,7 +22,7 @@ bzip2 -d boost.tar.bz2
 tar xopf boost.tar
 cp -a /boost_1_67_0/. /usr/local/boost
 cd boost_1_67_0
-bash -x ./bootstrap.sh —-prefix=/usr/local/boost
+bash ./bootstrap.sh —-prefix=/usr/local/boost
 ./b2 install
 cd $QME_NG_PATH
 make all
